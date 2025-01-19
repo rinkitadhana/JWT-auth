4119f15d19ada22f6c4adcb98ad6bc5da3a8f096
Rinkit Adhana <therinkit@gmail.com>
2025-01-20 01:23:04 +0530

    Set up Express server with dotenv configuration; added dependencies for Express and dotenv in package.json and package-lock.json.

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,16 @@
-console.log("Hello World!")
+import express from "express"
+import "dotenv/config"
+import { router } from "./routes/auth"
+require("dotenv").config()
+
+const app = express()
+const PORT = process.env.PORT
+
+app.get("/", (req, res) => {
+  res.send("Hello")
+})
+app.use("/api", router)
+
+app.listen(PORT, () => {
+  console.log(`Listening on port http://localhost:${PORT}`)
+})
