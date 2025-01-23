3fb508599211151c00907a36a721ed81ea5b9cbc
Rinkit Adhana <therinkit@gmail.com>
2025-01-24 01:57:42 +0530

    database-inchanced

--- src/config/databse.ts
+++ src/config/databse.ts
@@ -1,11 +1,8 @@
 import mongoose from "mongoose"
-import dotenv from "dotenv"
-
-dotenv.config()
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGODB_URI)
+    await mongoose.connect(process.env.MONGO_URI)
     console.log("MongoDB is Connected!")
   } catch (error) {
     console.log("MongoDB Connnection Error: ", error)
